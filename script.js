ee8cb64835c4d5d50ae4e3f1ff82c04b3cc23a5f
gulaitumanis <semvakacung@gmail.com>
2023-10-22 00:30:33 +0700

    website konime

---
+++ script.js
@@ -0,0 +1,12 @@
+// toggle class active
+
+const navMenu = document.querySelector
+
+('.navmenu');
+
+// Ketika hamburger menu di klik
+
+document.querySelector('#hamburger-menu').
+onclick = ()=> {
+    navMenu.classList.toggle('active');
+}
