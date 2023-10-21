0fe1e5476502b8a91efe05e2146f305980ceee5f
gulaitumanis <semvakacung@gmail.com>
2023-10-22 01:09:48 +0700

    added click function

--- script.js
+++ script.js
@@ -6,7 +6,18 @@ const navMenu = document.querySelector
 
 // Ketika hamburger menu di klik
 
+
 document.querySelector('#hamburger-menu').
 onclick = ()=> {
     navMenu.classList.toggle('active');
-}
+};
+
+
+// klik diluar nav untuk menghilangkan nav
+
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click',function(e) {
+    if (!hamburger.contains(e.target) && !navMenu.contains(e.target)) 
+    navMenu.classList.remove('active');
+});
